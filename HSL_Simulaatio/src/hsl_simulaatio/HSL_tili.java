1fd4e2c6f8c7b800c1ad9ba21295281a49db931b
jerrysalonen <32097936+jerrysalonen@users.noreply.github.com>
2017-09-26 15:08:02 +0300

    Kommentteja lisää

--- HSL_Simulaatio/src/hsl_simulaatio/HSL_tili.java
+++ HSL_Simulaatio/src/hsl_simulaatio/HSL_tili.java
@@ -9,8 +9,8 @@ package hsl_simulaatio;
 
 public class HSL_tili {
     private double arvo;
-    private int alueKoodi;
-    private int kausi;
+    private int alueKoodi; // vastaa kaupunkeja ja vyöhykkeitä
+    private int kausi; 
     
     HSL_tili(){
         
@@ -24,27 +24,33 @@ public class HSL_tili {
     
     public void setArvo(double arvo){
         this.arvo = arvo;
+        // arvo doublena, esim 4,25 tai 0,50 (euroina)
     }
     
     public void setAlue(int alueKoodi){
         // myöhemmin katsotaan täsmääkö aluekoodi alueiden kanssa
         this.alueKoodi = alueKoodi;
+        // aluekoodi kertoo vyöhykkeen, esim helsinki = 1, espoo = 2 jne..
     }
     
     public void setKausi(int kausi){
         this.kausi = kausi;
+        // setteri, joka kertoo kuinka monta päivää (int) kautta 
     }
     
     public double getArvo(){
         return this.arvo;
+        // palauttaa arvon
     }
     
     public int getAlue(){
         return this.alueKoodi;
+        // palauttaa aluekoodin
     }
     
     public int getKausi(){
         return this.kausi;
+        // palauttaa kauden keston päivinä
     }
     
 }
