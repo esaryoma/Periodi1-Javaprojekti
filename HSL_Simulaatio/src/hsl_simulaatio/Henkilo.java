db9c5c9468c6daae387efef5e6bed0fdbe346282
esaryoma <32095491+esaryoma@users.noreply.github.com>
2017-09-26 14:28:36 +0300

    Create Henkilo.java

---
+++ HSL_Simulaatio/src/hsl_simulaatio/Henkilo.java
@@ -0,0 +1,30 @@
+// Henkilö-luokka
+
+public class Henkilo {
+    
+  // Muuttujat:
+  private String etunimi;
+  private String sukunimi;
+  private int syntymavuosi;
+  private int syntymakuukausi;
+  private int syntymapaiva;
+  
+  
+  // Konstruktori sisältää kaikki Henkilo-luokan muuttujat parametreinä
+  Henkilo(String etunimi, String sukunimi, int syntymavuosi, int syntymakuukausi, int syntymapaiva){
+      this.etunimi = etunimi;
+      this.sukunimi = sukunimi;
+      this.syntymavuosi = syntymavuosi;
+      this.syntymakuukausi = syntymakuukausi;
+      this.syntymapaiva = syntymapaiva;
+            
+  }
+  
+  // Nollakonstruktori luo henkilö-olion oletusarvoilla
+  Henkilo (){
+      this.etunimi = M
+  }
+
+
+
+}    
