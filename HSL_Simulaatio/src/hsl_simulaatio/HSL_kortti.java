3b67aef7f56bc4c2e2bd51e58df6ff404e74bcee
esaryoma <32095491+esaryoma@users.noreply.github.com>
2017-09-29 11:32:30 +0300

    Maksukortin useimmat metodit kirjoitettu
    Kaikki rivit
    HSL_kortti (maksukortti) liitetään HSL_tiliin joko luonnin yhteydessä konstruktorissa tai erikseen setTili()-metodilla.
    Nyt maksukortin kautta voidaan hakea gettereillä infoa tilin ominaisuuksista ( getArvo(), getKausi() etc.)
    Maksukortin kautta voi lataa() ja maksa() -metodeilla lisäämään tai vähentämään tilillä olevaa arvoa.

--- HSL_Simulaatio/src/hsl_simulaatio/HSL_kortti.java
+++ HSL_Simulaatio/src/hsl_simulaatio/HSL_kortti.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hsl_simulaatio;
 
-/**
- *
- * @author Jerry Salonen
- */
+
 public class HSL_kortti {
+    // Kommunikoi asiakkaan HSL_tilin ja Kortinlukijan/Latauspisteen kanssa
     
-}
+    // Muuttujat
+    HSL_tili tili = new HSL_tili();
+    
+    
+    // Parametriton konstruktori
+    // Tarvitseeko tämä jotain muuta?
+    HSL_kortti(){
+    }
+    
+    // Parametrillinen konstruktori, joka luo kortin ja iskee sen suoraan kiinni tili-olioon
+    HSL_kortti(HSL_tili tili){
+        this.tili = tili;
+    }
+    
+    // Setteri
+    public void setTili(HSL_tili tili){
+        this.tili = tili;
+    
+    }
+    
+    // Getterit, jotka oikeastaan vaan kutsuvat kiinni olevan tilin Gettereitä
+    public double getArvo(){
+        return tili.getArvo();
+        // palauttaa tilin arvon
+    }
+    
+    public int getAlue(){
+        return tili.getAlue();
+        // palauttaa aluekoodin
+    }
+   
+    public int getKausi(){
+        return tili.getKausi();
+        // palauttaa kauden keston päivinä
+    }
+    
+    public int getAsiakaskunta(){
+        return tili.getAsiakaskunta();
+        // palauttaa asiakaskunnan
+    }
+    
+    // Muut metodit
+    // Lisätään tilille arvoa setArvo-metodin kautta
+    public void lataa(double lisays){
+        tili.setArvo(tili.getArvo() + lisays);
+    }
+    
+    // Ostoksen yhteydessä tarkistetaan onko tilillä tarpeeksi arvoa
+    public boolean maksa(double vahennys){
+        if(tili.getArvo() < vahennys){ // jos tilillä ei tarpeksi arvoa, vähennystä ei tapahdu
+            return false;                
+        }
+        else{
+            tili.setArvo(tili.getArvo() - vahennys); // jos arvoa löytyy, vähennys tehdään
+            return true;
+        }    
+        }
+    }
+    
+
