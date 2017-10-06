d6f8c2b6eaf832cd1541d905a20458566c77e5b7
jerrysalonen <32097936+jerrysalonen@users.noreply.github.com>
2017-10-06 16:59:02 +0300

    Lukijan ensimmäinen versio
    Metodi tarkistaKaupunki keskiössä

--- HSL_Simulaatio/src/hsl_simulaatio/HSL_lukija.java
+++ HSL_Simulaatio/src/hsl_simulaatio/HSL_lukija.java
@@ -4,5 +4,36 @@ Lukee kortilta tiedot ja kertoo voidaanko lippua hankkia.
 package hsl_simulaatio;
 
 public class HSL_lukija {
+    private HSL_kortti kortti;
+    private int kaupunki; // missä ollaan lipun ostamisen aikana
     
+    HSL_lukija(){
+        
+    }
+    
+    HSL_lukija(int kaupunki){
+        this.kaupunki = kaupunki;
+    }
+    
+    
+    // setteri kaupungille
+    public void setKaupunki(int kaupunki){
+        this.kaupunki = kaupunki;
+    }
+    
+    // getteri kaupungille
+    public int getKaupunki(){
+        return this.kaupunki;
+    }
+    
+    // kysyy kortilta tilin aluetta ja vertaa sitä lukijan kaupunkiin
+    public boolean tarkistaKaupunki(){
+        if (this.kaupunki == kortti.tarkistaKaupunki()){
+            return true;
+        } else if (kortti.tarkistaKaupunki()==3){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
