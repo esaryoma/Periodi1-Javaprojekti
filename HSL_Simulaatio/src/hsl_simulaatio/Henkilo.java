743c0e1423af8cc86f505fe39222e5982b2487fd
jerrysalonen <32097936+jerrysalonen@users.noreply.github.com>
2017-10-11 00:36:36 +0300

    final

--- HSL_Simulaatio/src/hsl_simulaatio/Henkilo.java
+++ HSL_Simulaatio/src/hsl_simulaatio/Henkilo.java
@@ -5,20 +5,11 @@ public class Henkilo {
     private String sukunimi;
     private String hetu;
     private int asiakasluokka;
-        /* 
-        Asiakasluokka
-        0 = lapsi 
-        1 = aikuinen
-        2 = opiskelija
-        3= eläkeläinen
-        */
     
-    // Nollakonstruktori
     public Henkilo(){
         
     }
     
-    // Konstruktori: ei asiakasluokkaa
     public Henkilo(String etunimi, String sukunimi, String hetu){
         
         this.etunimi = etunimi;
@@ -26,7 +17,6 @@ public class Henkilo {
         this.hetu = hetu;
     }
     
-    // Konstruktori: täydet tiedot
     public Henkilo(String etunimi, String sukunimi, String hetu, int asiakasluokka){
         
         this.etunimi = etunimi;
@@ -34,22 +24,22 @@ public class Henkilo {
         this.hetu = hetu;
         this.asiakasluokka = asiakasluokka;
     }
+    
     public String getEtunimi() {
         return etunimi;
     }
     public String getSukunimi() {
-        return sukunimi;
+        return this.sukunimi;
     }
     public String getHetu() {
-        return hetu;
+        return this.hetu;
     }
     public int getAsiakasluokka() {
-        return asiakasluokka;
+        return this.asiakasluokka;
     }
     
     @Override
     public String toString(){
-        return this.etunimi+" "+this.sukunimi;
+       return this.etunimi+" "+this.sukunimi;
     }
-       
 }
